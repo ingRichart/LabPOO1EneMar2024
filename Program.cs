fe78b061be9afd8e97d24b7af37d4333e8048271
Ricardo <ricardo.elizaldeh@un.edu.mx>
2024-01-12 19:05:00 -0600

    Determinar dos funciones que calculen la cantidad de números pares e impares, respectivamente que hay en un vector

--- Program.cs
+++ Program.cs
@@ -1,47 +1,96 @@
-﻿
+﻿/*
+Definir dos funciones que calculen la cantidad de números pares e impares, respectivamente que hay en un vector
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numeros = {1,2,3,4,5,6,7,8,9,9,6,3,1};
+
+        int cantidadPar = CalcularNumerosPares(numeros);
+        int cantidadImpar = CalcularNumerosImpares(numeros);
+
+        Console.WriteLine("Cantidad de numeros: " + numeros.Length);
+        Console.WriteLine("Cantidad de numeros par: " + cantidadPar);
+        Console.WriteLine("Cantidad de numeros impar: " + cantidadImpar);
+
+    }
+
+    public static int CalcularNumerosPares(int[] arreglo) {
+
+        int cantidad = 0;
+        foreach (var item in arreglo)
+        {
+            if (item % 2 == 0) 
+            {
+                Console.WriteLine("Numero par: " + item);
+                cantidad++;
+            }
+        }
+        return cantidad;
+
+    }
+
+        public static int CalcularNumerosImpares(int[] arreglo) {
+
+        int cantidad = 0;
+        foreach (var item in arreglo)
+        {
+            if (item % 2 != 0) 
+            {
+                Console.WriteLine("Numero impar: " + item);
+                cantidad++;
+            }
+        }
+        return cantidad;
+
+    }
+}
+
 /*
 Define una función que calcule el valor promedio de un vector numérico. La cantidad de elementos seran 5
 */
 
 
-    // Display title as the C# console calculator app.
-    Console.WriteLine("------------------------------------------------------------------------\n");
-    Console.WriteLine("Capturar de N números para determinar el promedio de los capturados \r");
-    Console.WriteLine("------------------------------------------------------------------------\n");
+// // Display title as the C# console calculator app.
+// Console.WriteLine("------------------------------------------------------------------------\n");
+// Console.WriteLine("Capturar de N números para determinar el promedio de los capturados \r");
+// Console.WriteLine("------------------------------------------------------------------------\n");
 
-    Console.WriteLine("Tamaño del arreglo \r");
-    int longitud = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Tamaño del arreglo \r");
+// int longitud = Convert.ToInt32(Console.ReadLine());
 
-    //Declaración de un variable de tipo arreglo con 5 posiciones
-    int[] numeros = new int[longitud];
+// //Declaración de un variable de tipo arreglo con 5 posiciones
+// int[] numeros = new int[longitud];
 
-    //Con un for estoy leyendo cada posición para llenar el arreglo con datos.
-    for (int i = 0; i < numeros.Length; i++)
-    {
-        Console.WriteLine("Escribe el numero de la posición : [" + i + "]");
-        numeros[i] = Convert.ToInt32(Console.ReadLine());
-    }
+// //Con un for estoy leyendo cada posición para llenar el arreglo con datos.
+// for (int i = 0; i < numeros.Length; i++)
+// {
+//     Console.WriteLine("Escribe el numero de la posición : [" + i + "]");
+//     numeros[i] = Convert.ToInt32(Console.ReadLine());
+// }
 
-    //Declarar una variables para obtener la sumatoria de los elementos del arreglo.
-    int suma = 0;
+// //Declarar una variables para obtener la sumatoria de los elementos del arreglo.
+// int suma = 0;
 
-    //Variable que me ayudará a recorrer todos los elementos del arreglo.
-    int j = 0;
+// //Variable que me ayudará a recorrer todos los elementos del arreglo.
+// int j = 0;
 
-    do
-    {
-        suma = suma + numeros[j];
-        j++;
-    } while (j < numeros.Length);
+// do
+// {
+//     suma = suma + numeros[j];
+//     j++;
+// } while (j < numeros.Length);
 
 
-    //Variable que es el resultado
-    double resultado = suma / numeros.Length;
-    
-    Console.WriteLine("El promedio de los numeros registrados es:" + resultado);
-    Console.Write("Fin del PRograma...");
+// //Variable que es el resultado
+// double resultado = suma / numeros.Length;
 
-    Console.ReadKey();
+// Console.WriteLine("El promedio de los numeros registrados es:" + resultado);
+// Console.Write("Fin del PRograma...");
+
+// Console.ReadKey();
 
 
 
@@ -84,4 +133,4 @@ Define una función que calcule el valor promedio de un vector numérico. La can
 //             Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
 //             break;
 //     }
-    // Wait for the user to respond before closing.
+// Wait for the user to respond before closing.
